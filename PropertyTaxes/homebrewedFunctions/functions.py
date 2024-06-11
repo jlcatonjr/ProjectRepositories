ee347bd4b81ff8e03e81bbeade7fe9f92455636f
jamescaton <jlcatonjr@gmail.com>
2024-06-11 17:38:05 -0500

    separate r2 and param visualizations to resolve multiplot axis conflict when num_rows are not equal

--- PropertyTaxes/homebrewedFunctions/functions.py
+++ PropertyTaxes/homebrewedFunctions/functions.py
@@ -181,79 +181,6 @@ def plot_r2(r2_df, r2s, key, variant):
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
-def dict_of_figs_to_dropdown_fig(figs, show_fig=True):
-    keys = figs.keys()
-    num_figs = len(keys)
-    num_traces = {key: len(fig.data) for key, fig in figs.items()}
-    show_traces = {key: [False for t in range(sum(num_traces.values()))] for key in keys}
-    start_trace_index = {key: sum(list(num_traces.values())[:i]) for i, key in enumerate(keys)}
-    end_trace_index = {key: sum(list(num_traces.values())[:i]) for i, key in enumerate(keys)}
-    
-    for key in keys:
-        show_traces[key][start_trace_index[key]:end_trace_index[key] + 1] = [True for t in range(num_traces[key])]
-    
-    # Initialize the combined figure
-    num_rows = max([int(fig.data[-1].xaxis.replace("x", "")) for name, fig in figs.items()])
-    combined_fig = make_subplots(rows=num_rows, cols=1, shared_xaxes=True)
-    
-    for key, fig in figs.items():
-        for trace in fig.data:
-            combined_fig.add_trace(trace)
-    
-    # Define buttons for the dropdown menu
-    dropdown_buttons = []
-    for key in keys:
-        fig = figs[key]
-        layout_json = fig.layout.to_plotly_json()
-        reset_layout = {}
-        # Remove layout settings for rows that do not exist in the current figure
-        current_num_rows = len(fig.data)
-                           
-        for i in range(current_num_rows + 1, num_rows + 1):
-            for prefix in ['yaxis', 'xaxis', 'annotations']:
-                if f'{prefix}{i}' in layout_json:
-                    reset_layout[f'{prefix}{i}'] = go.layout.XAxis(visible=False).to_plotly_json() if prefix.startswith('x') else go.layout.YAxis(visible=False).to_plotly_json()
-                    del layout_json[f'{prefix}{i}']
-            for j, annotation in enumerate(layout_json.get('annotations', [])):
-                if annotation.get('xref', '') == f'x{i}':
-                    reset_layout[f'annotations[{j}]'] = {'visible': False}
-                    del layout_json[f'annotations[{j}]']                    
-           
-        final_layout = {**layout_json, **reset_layout}
-        
-        dropdown_buttons.append({
-            "label": key,
-            "method": "update",
-            "args": [
-                {"visible": show_traces[key]},
-                final_layout
-            ]
-        })
-    
-    # Add dropdown menu to the figure layout
-    combined_fig.update_layout(
-        updatemenus=[
-            {
-                "buttons": dropdown_buttons,
-                "direction": "down",
-                "showactive": True,
-                "x": 0.5,  # Center the dropdown menu horizontally
-                "xanchor": "center",  # Set the anchor to the center
-                "y": 1.15,  # Position above the plot area
-                "yanchor": "top"  # Set the anchor to the top
-            }
-        ]
-    )
-
-    combined_fig.update_traces(visible=False)
-    for i, trace in enumerate(combined_fig.data):
-        trace.visible = show_traces[list(keys)[0]][i]
-    # Show combined figure
-    if show_fig:
-        combined_fig.show()
-        
-    return combined_fig
-
 def dict_of_figs_to_dropdown_fig(figs, show_fig = True):
     keys = figs.keys()
     num_figs = len(keys)
@@ -274,17 +201,14 @@ def dict_of_figs_to_dropdown_fig(figs, show_fig = True):
         # combined_fig.update_layout(fig.layout, overwrite=False)
 
         layout_json = fig.layout.to_plotly_json()   
-        # Remove layout settings for rows that do not exist in the current figure
+        # # Remove layout settings for rows that do not exist in the current figure
         current_num_rows = len(fig.data)
-        reset_layout = {}
+        # reset_layout = {}
         for i in range(current_num_rows + 1, num_rows + 1):
+
             for attr in ['yaxis', 'xaxis']:
-                reset_layout[f'{attr}{i}'] = {}
-            for j in range(len(layout_json.get('annotations', []))):
-                reset_layout[f'annotations[{j}]'] = {}
-                reset_layout[f'annotations[{j}]'] = {'visible': False}
-        
-        final_layout = {**layout_json, **reset_layout}
+                fig.layout[f'{attr}{i}'] = {}
+
 
 
     # Define buttons for the dropdown menu
@@ -295,41 +219,11 @@ def dict_of_figs_to_dropdown_fig(figs, show_fig = True):
             "method": "update",
             "args": [
                 {"visible": show_traces[key]},
-                final_layout#**figs[key].layout.to_plotly_json()}
+                {**figs[key].layout.to_plotly_json()}
             ]
         } for key in keys
     ]
-   
-    # dropdown_buttons = []
-    # for key in keys:
-    #     fig = figs[key]
-    #     layout_json = fig.layout.to_plotly_json()
-    #     reset_layout = {}
-    #     # Remove layout settings for rows that do not exist in the current figure
-    #     current_num_rows = len(fig.data)
-                           
-    #     for i in range(current_num_rows + 1, num_rows + 1):
-    #         for key in layout_json.keys():
-    #             if f'{key}{i}' in layout_json:
-    #                 reset_layout[f'{key}{i}'] = {'visible': False}
-
-    #                 del layout_json[f'{key}{i}']
-    #         for j, annotation in enumerate(layout_json.get('annotations', [])):
-    #             if f'annotations[{j}]' in layout_json and annotation.get('xref', '') == f'x{i}':
-    #                 reset_layout[f'annotations[{j}]'] = {'visible': False}
-    #                 del layout_json[f'annotations[{j}]']                    
-
-           
-    #     final_layout = {**layout_json, **reset_layout}
-        
-    #     dropdown_buttons.append({
-    #         "label": key,
-    #         "method": "update",
-    #         "args": [
-    #             {"visible": show_traces[key]},
-    #             final_layout
-    #         ]
-    #     })
+    combined_fig.layout = {**figs[list(figs.keys())[0]].layout.to_plotly_json()}
     # Add dropdown menu to the figure layout
     combined_fig.update_layout(
             updatemenus=[
@@ -353,3 +247,5 @@ def dict_of_figs_to_dropdown_fig(figs, show_fig = True):
         combined_fig.show()
         
     return combined_fig
+
+
